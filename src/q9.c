85b02c465193df7a210f86264097374c0e2f6f93
unknown <ramitkc750@gmail.com>
2026-04-29 21:36:38 +0545

    initial submission

--- src/q9.c
+++ src/q9.c
@@ -3,28 +3,28 @@
 #include <stdio.h>
 #include <math.h>
 
-struct Point {
-    float x;
-    float y;
-};
+typedef struct {
+    double x;
+    double y;
+} Point;
 
-float distance(struct Point p1, struct Point p2) {
-    float dx = p2.x - p1.x;
-    float dy = p2.y - p1.y;
-
+double distance(Point p1, Point p2) {
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
     return sqrt(dx * dx + dy * dy);
 }
 
 int main() {
-    struct Point p1, p2;
+    Point p1, p2;
 
-    printf("Enter x and y for point 1: ");
-    scanf("%f %f", &p1.x, &p1.y);
+    printf("Enter coordinates of point 1 (x y): ");
+    scanf("%lf %lf", &p1.x, &p1.y);
 
-    printf("Enter x and y for point 2: ");
-    scanf("%f %f", &p2.x, &p2.y);
+    printf("Enter coordinates of point 2 (x y): ");
+    scanf("%lf %lf", &p2.x, &p2.y);
 
-    printf("Distance: %.2f\n", distance(p1, p2));
+    double dist = distance(p1, p2);
+    printf("Distance between the points: %.2lf\n", dist);
 
     return 0;
 }
